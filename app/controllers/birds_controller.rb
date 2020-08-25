b6962ca9453d5ad984f3c2e8768623eb3e4a23b1
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-08-25 04:30:05 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -6,9 +6,11 @@ class BirdsController < ApplicationController
  
   def show
     bird = Bird.find_by(id: params[:id])
-    render json: bird
+    #render json: bird
+    render json: {id: bird.id, name: bird.name, species: bird.species } 
   end
 end
+#can pick and choose what you'd like to send 
 
 #No need for instance variables anymore, since we're immediately rendering birds and bird to JSON and are not going to be using ERB.
 
