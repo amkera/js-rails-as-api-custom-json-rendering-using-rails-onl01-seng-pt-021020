b7c052321354ccaf8ba558797f873cb7e3b6a317
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-08-25 04:37:15 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -19,8 +19,5 @@ class BirdsController < ApplicationController
     #Rather than having to spell out each key, the Hash slice method returns a new hash with only the keys that are passed into slice
     #While slice works fine for a single hash, as with bird, it won't work for an array of hashes like the one we have in our index action:
     end
+  end 
 end
-#can pick and choose what you'd like to send 
-
-#No need for instance variables anymore, since we're immediately rendering birds and bird to JSON and are not going to be using ERB.
-
