4d1e66c5887645c032c5f59d13108c54c4bae11f
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-08-25 04:33:37 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -3,7 +3,7 @@ class BirdsController < ApplicationController
     birds = Bird.all
     render json: birds, only: [:id, :name, :species]
     
-    # OR: 
+    # OR: render json: birds, except: [:created_at, :updated_at]
     
     #While slice works fine for a single hash, as with bird, it won't work for an array of hashes like the one we have in our index action.
     #In this case, we can add in the only: option directly after listing an object we want to render to JSON:
