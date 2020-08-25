f1d28ba369522387cb49f6e1b1034225d1314855
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-08-25 04:32:38 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,17 +1,18 @@
 class BirdsController < ApplicationController
   def index
     birds = Bird.all
-    render json: birds
+    render json: birds, only: [:id, :name, :species]
+    
   end
  
   def show
     bird = Bird.find_by(id: params[:id])
     #render json: bird
     #render json: {id: bird.id, name: bird.name, species: bird.species } 
-    #OR: render json: bird.slice(:id, :name, :species)
+    render json: bird.slice(:id, :name, :species)
     #Rather than having to spell out each key, the Hash slice method returns a new hash with only the keys that are passed into slice
     #While slice works fine for a single hash, as with bird, it won't work for an array of hashes like the one we have in our index action:
-    render json: birds, only: [:id, :name, :species]
+    
   end
 end
 #can pick and choose what you'd like to send 
